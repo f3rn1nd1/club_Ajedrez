87690cbeec490f3aace723ce46e872e77d315dc0
JacqueRobles <116358744+JacqueRobles@users.noreply.github.com>
2023-09-08 04:20:14 +0000

    primer desarrollo login

---
+++ loging.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Club Ajedrez</title>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilos.css">
+
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="login.php" method="post">
+        <input type="text" name= "email" placeholder="Ingrese su e-mail">
+        <input type="password" name="password" placeholder="Ingrese su contraseña">
+        <input type="submit" value="Enviar">
+    
+    </form>
+</body>
+</html>
